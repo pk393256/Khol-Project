f7e0edd570c5c0c058ddc2c4c300024091e34069
naseeb-shah <102802675+naseeb-shah@users.noreply.github.com>
2022-04-16 20:36:44 +0530

    deen

--- singin.js
+++ singin.js
@@ -14,3 +14,32 @@ function saveuser(){
      localStorage.setItem('usercollection', JSON.stringify(sai))
      console.log(sai)
 }
+
+      var x=document.getElementById('s').addEventListener('click',function(){
+       
+          if(document.getElementById('a').style.display=='block'){
+            document.getElementById('a').style.display='none'
+          }else{
+            document.getElementById('a').style.display='block'
+
+          }
+          document.getElementById('b').style.display='none'
+      })
+     
+      var x=document.getElementById('deen').addEventListener('click',function(){
+        
+        if(document.getElementById('b').style.display=='block'){
+            document.getElementById('b').style.display='none'
+          }else{
+            document.getElementById('b').style.display='block'
+
+          }
+          document.getElementById('a').style.display='none'
+      })
+      var x=document.getElementById('sai').addEventListener('click',function(){
+       
+            document.getElementById('b').style.display='none'
+       
+          document.getElementById('a').style.display='none'
+      })
+     
