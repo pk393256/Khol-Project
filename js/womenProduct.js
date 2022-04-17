9e93633cb544d15c8770bef00d3d66c2df096073
Pappu Kumar <digitalpappu101@gmail.com>
2022-04-17 11:54:19 +0530

    womenProduct js file updated

--- js/womenProduct.js
+++ js/womenProduct.js
@@ -1,1 +1,30 @@
 //womenProduct js 
+var span=document.getElementsByClassName('spam');
+var div=document.getElementsByClassName('cdiv');
+let l=0;
+span[1].onclick=()=>{
+    l++;
+    for(var i of div){
+    if(l==0){i.style.left="0";}
+    if(l==1){i.style.left="-740px";}
+    if(l==2){i.style.left="-1480px";}
+    if(l==3){i.style.left="-2220px";}
+    if(l==4){i.style.left="-2960px";}
+    if(l>4){l=4;}
+    if(l>=12){l=0};
+ }
+}
+span[0].onclick=()=>{
+l--;
+
+for(var i of div){
+   
+if(l==0){i.style.left="0";}
+if(l==1){i.style.left="-740px";}
+if(l==2){i.style.left="-1480px";}
+if(l==3){i.style.left="-2220px";}
+if(l==4){i.style.left="-2960px";}
+if(l>4){l=4;}
+if(l>=12){l=0};
+}
+}
