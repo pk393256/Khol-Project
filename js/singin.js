6be2eb5ca1350786ea7a3fb47a8b86e292a19c4b
Pappu Kumar <digitalpappu101@gmail.com>
2022-04-17 12:23:38 +0530

    updated singin html css js file

---
+++ js/singin.js
@@ -0,0 +1,47 @@
+if(JSON.parse(localStorage.getItem('usercollection'))!==null){
+    sai=JSON.parse(localStorage.getItem('usercollection'))
+  }else{
+    sai=[]
+  }
+  
+  function saveuser(){
+   
+   var username =  {
+       f :document.getElementById('fname').value,
+     l :document.getElementById('lname').value,
+     e :document.getElementById('email').value,
+     p :document.getElementById('Password').value}
+     sai.push(username)
+    
+     localStorage.setItem('usercollection', JSON.stringify(sai))
+     alert("Successful!")
+     window.open('login.html')
+  }
+  var x=document.getElementById('s').addEventListener('click',function(){
+       
+       if(document.getElementById('a').style.display=='block'){
+         document.getElementById('a').style.display='none'
+       }else{
+         document.getElementById('a').style.display='block'
+  
+       }
+       document.getElementById('b').style.display='none'
+   })
+  
+   var x=document.getElementById('deen').addEventListener('click',function(){
+     
+     if(document.getElementById('b').style.display=='block'){
+         document.getElementById('b').style.display='none'
+       }else{
+         document.getElementById('b').style.display='block'
+  
+       }
+       document.getElementById('a').style.display='none'
+   })
+   var x=document.getElementById('sai').addEventListener('click',function(){
+    
+         document.getElementById('b').style.display='none'
+    
+       document.getElementById('a').style.display='none'
+   })
+  
