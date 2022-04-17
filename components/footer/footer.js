1552aa0a764b2959142f7e61c187f71c29c540f4
Pappu Kumar <digitalpappu101@gmail.com>
2022-04-17 09:58:55 +0530

    footer code updated

---
+++ components/footer/footer.js
@@ -0,0 +1,93 @@
+export default async function footer(){
+    return  `<div id = "mainp">
+    <div id = "containerp">
+        <div id = "social">
+            <h4>CONNECT<br>WITH US</h4>
+            <p><i class="fa-brands fa-facebook-f"></i></p>
+            <p><i class="fa-brands fa-twitter"></i></p>
+            <p><i class="fa-brands fa-pinterest-p"></i></p>
+            <p><i class="fa-brands fa-instagram"></i></p>
+            <p><i class="fa-brands fa-youtube"></i></p>
+        </div>
+        <div id = "topright">
+        <div id = "app">
+            <h4>GET OUR<br>APP</h4>
+            <img src = "https://media.kohlsimg.com/is/image/kohls/AppStore-qr_code2020?scl=1&fmt=png8">
+        </div>
+        <div id = "scantext">
+            <p>Scan the code to download
+                the Kohl's App today.</p>
+            <input type = "button" value = "Learn More">
+        </div>
+    </div>
+    </div>
+    <div id = "footergrid">
+        <div class = "gridcontent">
+            <h5>CUSTOMER SERVICE</h5>
+            <p>Contact Us</p>
+            <p>Shipping</p>
+            <p>Returns</p>
+            <p>Rebates</p>
+            <p>Recalls</p>
+            <p>Product Guides</p>
+            <p>Kohl's Blog</p>
+            <p>Kohl's Cash</p>
+        </div>
+        <div class = "gridcontent">
+            <h5>SHOP KOHL’S</h5>
+            <p>Get 15% off when you
+                sign up for our emails</p>
+            <p>Site Map</p>
+            <p>Store Locator</p>
+            <p>Gift Cards</p>
+            <p>Kohl’s Coupons</p>
+         </div>
+        <div class = "gridcontent">
+            <h5>MY ACCOUNT</h5>
+            <p>Sign In</p>
+            <p>My Account</p>
+            <p>Update Password</p>
+            <p>Order Status</p>
+            <p>Rewards</p>
+            <p>Gift Card Balance &
+                Kohl’s Cash Balance</p>
+        </div>
+        <div class = "gridcontent">
+            <h5>KOHL’S CARD</h5>
+            <p>Benefits</p>
+            <p>Pay & Manage Card</p>
+            <p>Apply for a Kohl’s Card</p>
+            <p>See if you prequalify</p>
+        </div>
+        <div class = "gridcontent">
+            <h5>ABOUT KOHL’S</h5>
+            <p>Our Websites</p>
+            <p>Community</p>
+            <p>Diversity & Inclusion</p>
+            <p>Sustainability</p>
+            <p>Careers</p>
+            <p>Apply for Seasonal Jobs</p>
+            <p>Associate Services</p>
+            <p>Investor Relations</p>
+            <p>Affiliate Program</p>
+            <p>Advertise with Us</p>
+        </div>
+    </div>
+    <div id = "disclaimer">
+        <p>© 2022 Kohl’s, Inc.<br>
+            KOHL’S® and Kohl’s brand names are trademarks owned by KIN, Inc.<br>
+            All rights reserved.</p>
+        <p>Android, Google Play and the Google Play logo are trademarks of Google Inc. App Store is a service mark of Apple Inc.</p>
+        
+    <div id = "lastline">
+        <h6><u>Legal Notices</u>,&nbsp;</h6>
+        <h6><u>Privacy Policy</u>,&nbsp;</h6>
+        <h6><u>California Privacy Policy</u>,&nbsp;</h6>
+        <h6><u>CA-Do Not Sell My Personal Information</u>,&nbsp;</h6>
+        <h6><u>About Our Ads</u>,&nbsp;</h6>
+        <h6><u>California Transparency in Supply Chains Act</u>.</h6>
+    </div>
+    
+    </div>
+</div>`
+}
